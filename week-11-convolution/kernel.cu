337bf574d2c268ee996fdec0bf754ea1cf98544b
Nithish Kumar <nithssh@protonmail.com>
2022-06-30 22:07:54 +0530

    Init commit

---
+++ week-11-convolution/kernel.cu
@@ -0,0 +1,180 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+// Mask param not needed as it is fixed for all inputs.
+__global__ void
+row_convolution(float* mat_input1, float* mat_output1, int mat_datasize, int mat_dim)
+{
+    int blockNum = blockIdx.x;
+    int threadNum = threadIdx.x;
+    int globalThreadId = blockNum * (blockDim.x) + threadNum;
+
+    int row_index = globalThreadId / mat_dim;
+    int col_index = globalThreadId % mat_dim;
+
+    if (row_index < mat_dim && col_index < mat_dim)
+    {
+        float leftVal = col_index - 1 < 0 ? 0.0f : mat_input1[globalThreadId - 1]; // padding
+        float rightVal = col_index + 1 >= mat_dim ? 0.0f : mat_input1[globalThreadId + 1];
+        mat_output1[globalThreadId] = (leftVal * -1) + (mat_input1[globalThreadId] * 2.5) + (rightVal * -1);
+    }
+}
+
+__global__ void
+col_convolution(float* mat_input1, float* mat_output1, int mat_datasize, int mat_dim)
+{
+    int blockNum = blockIdx.x;
+    int threadNum = threadIdx.x;
+    int globalThreadId = blockNum * (blockDim.x) + threadNum;
+
+    int row_index = globalThreadId / mat_dim;
+    int col_index = globalThreadId % mat_dim;
+
+    if (row_index < mat_dim && col_index < mat_dim)
+    {
+        float topVal = row_index - 1 < 0 ? 0.0f : mat_input1[(mat_dim * (row_index - 1)) + col_index];
+        float bottomVal = row_index + 1 >= mat_dim ? 0.0f : mat_input1[(mat_dim * (row_index + 1)) + col_index];
+        mat_output1[globalThreadId] += (topVal * -1) + mat_input1[globalThreadId] * 2.5 + (bottomVal * -1);
+    }
+}
+
+void print_matrix(float* A, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            printf("%.1f ", A[i * n + j]); // single decimal precision as specified
+        printf("\n");
+    }
+
+}
+
+int main(void)
+{
+    cudaError_t err = cudaSuccess;
+    
+    int i, j, k;
+    int testCases;
+    scanf("%d", &testCases);
+    int testCasesOriginal = testCases; // used at end to print results
+
+    // using two primitive arrays instead of one struct array to store results
+    float **results = new float *[testCases];
+    int *dims = new int[testCases];
+
+    while (testCases--)
+    {
+        int mat_dim;
+        scanf("%d", &mat_dim);
+        int mat_num_eles = mat_dim * mat_dim;
+        size_t mat_size = mat_num_eles * sizeof(float);
+
+        /* populate code for allocating host memory */
+        float *h_mat_input1 = (float*)malloc(mat_size);
+        float *h_mat_output1 = (float*)malloc(mat_size);
+
+        int mat_conv_dim = 3;
+        int mat_conv_num_eles = mat_conv_dim*mat_conv_dim;
+        size_t mat_conv_size = mat_conv_num_eles*sizeof(float);
+
+        if (h_mat_input1 == NULL || h_mat_output1 == NULL)
+        {
+            fprintf(stderr, "Failed to allocate host vectors!\n");
+            exit(EXIT_FAILURE);
+        }
+
+        for (i = 0; i < mat_num_eles; i++)
+        {
+            j = i / mat_dim;
+            k = i % mat_dim;
+
+            scanf("%f", &h_mat_input1[mat_dim * j + k]);
+        }
+
+        float *d_mat_input1 = NULL;
+        err = cudaMalloc((void **)&d_mat_input1, mat_size);
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to allocate device vector d_mat_input1 (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
+
+        float *d_mat_output1 = NULL;
+        err = cudaMalloc((void **)&d_mat_output1, mat_size);
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to allocate device vector d_mat_output1 (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
+
+
+        err = cudaMemcpy(d_mat_input1, h_mat_input1, mat_size, cudaMemcpyHostToDevice);
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to copy vector h_mat_input1 from host to device (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
+
+        int mat_block_dim = 16;
+        int mat_grid_dim = mat_num_eles / mat_block_dim > 0 ? ceilf((float)mat_num_eles / mat_block_dim) : 1;
+        row_convolution<<<mat_grid_dim, mat_block_dim>>>(d_mat_input1, d_mat_output1, mat_size, mat_dim);
+        col_convolution<<<mat_grid_dim, mat_block_dim>>>(d_mat_input1, d_mat_output1, mat_size, mat_dim);
+
+        err = cudaGetLastError();
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to launch process_kernel2 kernel (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
+
+
+        err = cudaMemcpy(h_mat_output1, d_mat_output1, mat_size, cudaMemcpyDeviceToHost);
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to copy vector d_mat_output1 from device to host (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
+
+
+        err = cudaFree(d_mat_input1);
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to free device vector d_mat_input1 (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
+
+        err = cudaFree(d_mat_output1);
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to free device vector d_mat_output1 (error code %s)!\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
+
+        free(h_mat_input1);
+        results[testCases] = h_mat_output1;
+        dims[testCases] = mat_dim;
+
+        err = cudaDeviceReset();
+        if (err != cudaSuccess)
+        {
+            fprintf(stderr, "Failed to deinitialize the device! error=%s\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
+
+    }
+
+    // print the results and then free them
+    for (int i = testCasesOriginal-1; i >= 0; --i)
+    {
+        print_matrix(results[i], dims[i], dims[i]);
+        free(results[i]);
+    }
+
+    return 0;
+}
---
+++ week11-cascading-triple/kernel.cu
@@ -0,0 +1,288 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+void print_array(float* A, int N)
+{
+    for (int i = 0; i < N; i++)
+        printf("%.2f ", A[i]);
+    printf("\n");
+}
+
+
+__global__ void
+compute_kernel1(float* input1, float* input2, float* output, int datasize)
+{
+    int numElements = datasize / sizeof(float);
+
+    // Write code for i
+    int innerThreadId = threadIdx.x + (threadIdx.y * blockDim.x) + (threadIdx.z * (blockDim.x * blockDim.y));
+    int globalBlockId = blockIdx.x + (blockIdx.y * gridDim.x) + (blockIdx.z * (gridDim.x * gridDim.y));
+    int i = (globalBlockId * (blockDim.x * blockDim.y * blockDim.z)) + innerThreadId;
+
+    if (i < numElements)
+    {
+        //Write code for compute
+        output[i] = (input1[i] * input2[i]) * 4;
+    }
+}
+
+
+__global__ void
+compute_kernel2(float* input, float* output, int datasize)
+{
+    int numElements = datasize / sizeof(float);
+
+    // Write code for i
+    int innerThreadId = threadIdx.x + (threadIdx.y * blockDim.x) + (threadIdx.z * (blockDim.x * blockDim.y));
+    int globalBlockId = blockIdx.x + (blockIdx.y * gridDim.x) + (blockIdx.z * (gridDim.x * gridDim.y));
+    int i = (globalBlockId * (blockDim.x * blockDim.y * blockDim.z)) + innerThreadId;
+
+    if (i < numElements)
+    {
+        //Write code for compute
+        output[i] = input[i] * input[i];
+    }
+}
+
+
+__global__ void
+compute_kernel3(float* input, float* output, int datasize)
+{
+    int numElements = datasize / sizeof(float);
+
+    // Write code for i
+    int innerThreadId = threadIdx.x + (threadIdx.y * blockDim.x) + (threadIdx.z * (blockDim.x * blockDim.y));
+    int globalBlockId = blockIdx.x + (blockIdx.y * gridDim.x) + (blockIdx.z * (gridDim.x * gridDim.y));
+    int i = (globalBlockId * (blockDim.x * blockDim.y * blockDim.z)) + innerThreadId;
+
+    if (i < numElements)
+    {
+        output[i] = input[i] * i;
+        //Write code for compute
+    }
+}
+
+
+
+int main(void)
+{
+    cudaError_t err = cudaSuccess;
+
+    int numElements = 16384;
+    size_t size = numElements * sizeof(float);
+
+    float* h_input1 = (float*)malloc(size);
+
+    float* h_input2 = (float*)malloc(size);
+
+    float* h_output1 = (float*)malloc(size);
+
+    float* h_output2 = (float*)malloc(size);
+
+    float* h_output3 = (float*)malloc(size);
+
+    if (h_input1 == NULL || h_input2 == NULL || h_output1 == NULL || h_output2 == NULL || h_output3 == NULL)
+    {
+        fprintf(stderr, "Failed to allocate host vectors!\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    for (int i = 0; i < numElements; ++i)
+    {
+        scanf("%f", &h_input1[i]);
+
+    }
+    for (int i = 0; i < numElements; ++i)
+    {
+        scanf("%f", &h_input2[i]);
+
+    }
+
+
+
+    float* d_input1 = NULL;
+    err = cudaMalloc((void**)&d_input1, size);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector d_input1 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    float* d_input2 = NULL;
+    err = cudaMalloc((void**)&d_input2, size);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector d_input2 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    float* d_output1 = NULL;
+    err = cudaMalloc((void**)&d_output1, size);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector h_output1 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    float* d_output2 = NULL;
+    err = cudaMalloc((void**)&d_output2, size);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector h_output2 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    float* d_output3 = NULL;
+    err = cudaMalloc((void**)&d_output3, size);
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device vector h_output3 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // printf("Copy input data from the host memory to the CUDA device\n");
+    err = cudaMemcpy(d_input1, h_input1, size, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector h_input1 from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(d_input2, h_input2, size, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector h_input2 from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    //Complete Code for launching compute_kernel1
+    dim3 k1_dimGrid(4, 2, 2);
+    dim3 k1_dimBlock(32, 32, 1);
+    compute_kernel1<<<k1_dimGrid, k1_dimBlock>>>(d_input1, d_input2, d_output1, size);
+
+    err = cudaGetLastError();
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to launch process_kernel1 kernel (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    //Complete Code for launching compute_kernel2
+    dim3 k2_dimGrid(2, 8, 1);
+    dim3 k2_dimBlock(8, 8, 16);
+    compute_kernel2<<<k2_dimGrid, k2_dimBlock>>>(d_output1, d_output2, size);
+    
+    err = cudaGetLastError();
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to launch process_kernel2 kernel (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    //Complete Code for launching compute_kernel3 
+    dim3 k3_dimGrid(16, 1, 1);
+    dim3 k3_dimBlock(128, 8, 1);
+    compute_kernel3<<<k3_dimGrid, k3_dimBlock>>>(d_output2, d_output3, size);
+
+
+    err = cudaGetLastError();
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to launch process_kernel3 kernel (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    
+
+    // printf("Copy output data from the CUDA device to the host memory\n");
+    err = cudaMemcpy(h_output1, d_output1, size, cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector d_output1 from device to host (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(h_output2, d_output2, size, cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector d_output2 from device to host (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaMemcpy(h_output3, d_output3, size, cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy vector d_output3 from device to host (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+
+
+
+    print_array(h_output3, numElements);
+
+
+    err = cudaFree(d_input1);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to free device vector d_input1 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaFree(d_input2);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to free device vector d_input2 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaFree(d_output1);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to free device vector d_output1 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaFree(d_output2);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to free device vector d_output2 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaFree(d_output3);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to free device vector d_output3 (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    free(h_input1);
+    free(h_input2);
+    free(h_output1);
+    free(h_output2);
+    free(h_output3);
+
+    err = cudaDeviceReset();
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to deinitialize the device! error=%s\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+
+    return 0;
+}
+
